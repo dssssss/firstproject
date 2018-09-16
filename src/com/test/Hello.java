6fd7937af50a8c704439394b807daa6f08339fc1
dssssss <940554665@qq.com>
2018-09-16 23:31:27 +0800

    Initial commit

---
+++ src/com/test/Hello.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import java.util.ArrayList;
+
+/**
+ * Created by hasee on 2018/9/16.
+ */
+public class Hello {
+    //565454545
+    public static void main(String[] args){
+        System.out.println("hello,world");
+        ArrayList list = new ArrayList<>();
+
+    }
+
+}
